d428f81fd5baab97e0c13e7fd369adbcc492f4c1
eloymartos <115500079+eloymartos@users.noreply.github.com>
2024-01-12 18:04:50 +0100

    base

---
+++ app/src/main/java/com/example/room/addtasks/ui/TasksScreen.kt
@@ -0,0 +1,13 @@
+package com.example.room.addtasks.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TasksScreen(){
+    Column(Modifier.fillMaxSize()) {
+
+    }
+}
